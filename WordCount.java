c5f77e52e6abab316577f7fdf320c8635d1679c6
Aurobind Iyer <aurobindiyer@gmail.com>
2018-07-20 19:40:03 +0530

    Updated file

--- WordCount.java
+++ WordCount.java
@@ -18,56 +18,60 @@ import org.apache.hadoop.mapreduce.Counter;
  */
 
 public class WordCount{
-public static class WordCountMapper extends Mapper<LongWritable, Text, Text, Text>
-{
+	
+	public static class WordCountMapper extends Mapper<LongWritable, Text, Text, Text>{
+		
         private Text word = new Text();
-        public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException
-        {
+        
+		public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException{
             String line = value.toString();
             StringTokenizer tokenizer = new StringTokenizer(line);
             String docID="";
-            if(line.contains("\t"))
-                {
+            
+			if(line.contains("\t")){
                     docID = tokenizer.nextToken().trim();
                 }
-            while(tokenizer.hasMoreTokens())
-            {
+				
+            while(tokenizer.hasMoreTokens()){
                 word.set(tokenizer.nextToken());
                 context.write(word,new Text(docID));
             }
         }
-}
-public static class WordCountReducer extends Reducer<Text, Text, Text, Text>
-{
-        public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException
-        {
-        HashMap<String, Integer>  hm = new HashMap<String, Integer>();
-		String result="";
-        for( Text a : values )
-        {
-            String entry = a.toString();
-            if(hm.containsKey(entry))
-                {   int incr = hm.get(entry) + 1;
+	}
+	
+	public static class WordCountReducer extends Reducer<Text, Text, Text, Text>{
+		
+        public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException{
+			HashMap<String, Integer>  hm = new HashMap<String, Integer>();
+			String result="";
+        
+			for(Text a : values){
+				String entry = a.toString();
+            
+				if(hm.containsKey(entry)){ 
+					int incr = hm.get(entry) + 1;
                     hm.put(entry,incr);
                 }
-            else
-                {
-                    hm.put(entry,1);
-                }
+				else{
+					hm.put(entry,1);
+				}
+			}
+			
+			for( String k : hm.keySet() ){
+				result = result + k + ":" + hm.get(k).toString() + " ";
+			}
+			
+			context.write(key, new Text(result));
         }
-        for( String k : hm.keySet() )
-        {
-             result = result + k + ":" + hm.get(k).toString() + " ";
-        }
-        context.write(key, new Text(result));
-        }
-}
+	}
+	
     public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
         if (args.length != 2){
             System.err.println("Usage: Word Count <input path> <output path>");
             System.exit(-1);
         }
-        Configuration conf = new Configuration();
+        
+		Configuration conf = new Configuration();
         Job job = Job.getInstance(conf,"word count");
         job.setJarByClass(WordCount.class);
         job.setMapperClass(WordCountMapper.class);
@@ -77,5 +81,5 @@ public static class WordCountReducer extends Reducer<Text, Text, Text, Text>
         FileInputFormat.addInputPath(job, new Path(args[0]));
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
         System.exit(job.waitForCompletion(true) ? 0 : 1);
-        }
+	}
 }
